fe93f52d2097ad725d823a5b359debcc89b2c849
BRB3D <samuraix221@hotmail.com>
2021-10-30 00:05:45 -0400

    Recursive function, with one for loop

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0; 
+ for (let i = 0; i < array.length; i++) {
+  if (Array.isArray(array[i])) {
+    sum += sumItems(array[i]); 
+  }else  {
+  sum += array[i];
+  }
+ }
+  return sum; 
 }
 
+let baseCase = [1, [2, [3]], 4, 5]
+console.log(sumItems(baseCase))
 module.exports = sumItems;
+
+
